f0810c801e63ae158b6f747a1a0d9697279d4606
Soe Moe Naing <soemoen900@gmail.com>
2026-06-07 09:07:32 +0630

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,37 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(
-          color: Colors.white,
-          child: Text(
-            'Hello, World!',
-            style: TextStyle(fontSize: 24, color: Colors.teal),
-          ),
-        ),
+        body: SafeArea(
+            child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: NetworkImage(
+                  'https://cdn.iconscout.com/icon/free/png-256/free-avatar-icon-svg-download-png-456322.png'),
+            ),
+            Text(
+              'Soe Moe Naing',
+              style: TextStyle(
+                fontSize: 23.0,
+                color: Colors.white,
+              ),
+            ),
+            Text('Flutter Developer',
+                style: TextStyle(
+                  fontSize: 13.0,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                )),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.teal.shade100,
+              ),
+            ),
+          ],
+        )),
       ),
     );
   }
