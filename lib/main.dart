6271c8e860120c7fb41be52640c1bef59665b904
Soe Moe Naing <soemoen900@gmail.com>
2026-06-08 09:42:00 +0630

    Lesson Complete Mi Card

--- lib/main.dart
+++ lib/main.dart
@@ -41,49 +41,53 @@ class MyApp extends StatelessWidget {
                 color: Colors.teal.shade100,
               ),
             ),
-            Container(
+            Card(
               color: Colors.white,
               margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              padding: EdgeInsets.all(10.0),
-              child: Row(
-                children: <Widget>[
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+959973465722',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 15.0,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(children: <Widget>[
                   Icon(
-                    Icons.phone,
+                    Icons.email,
                     color: Colors.teal,
                   ),
                   SizedBox(
                     width: 10.0,
                   ),
                   Text(
-                    '+959973465722',
+                    'soemoen900@gmail.com',
                     style: TextStyle(
                       color: Colors.teal.shade900,
                       fontSize: 15.0,
                     ),
                   )
-                ],
+                ]),
               ),
-            ),
-            Container(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              padding: EdgeInsets.all(10.0),
-              child: Row(children: <Widget>[
-                Icon(
-                  Icons.email,
-                  color: Colors.teal,
-                ),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text(
-                  'soemoen900@gmail.com',
-                  style: TextStyle(
-                    color: Colors.teal.shade900,
-                    fontSize: 15.0,
-                  ),
-                )
-              ]),
             )
           ],
         )),
