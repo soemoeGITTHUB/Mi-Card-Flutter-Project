65b3402cde61157928c892cd5b7184420436234f
Soe Moe Naing <soemoen900@gmail.com>
2026-06-08 09:41:41 +0630

    Lesson Complete

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,50 @@ class MyApp extends StatelessWidget {
                 color: Colors.teal.shade100,
               ),
             ),
+            Container(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              padding: EdgeInsets.all(10.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    '+959973465722',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontSize: 15.0,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              padding: EdgeInsets.all(10.0),
+              child: Row(children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.teal,
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  'soemoen900@gmail.com',
+                  style: TextStyle(
+                    color: Colors.teal.shade900,
+                    fontSize: 15.0,
+                  ),
+                )
+              ]),
+            )
           ],
         )),
       ),
